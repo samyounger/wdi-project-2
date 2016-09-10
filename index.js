eb16d51f9dd6c1d6f71d76346222bff5f9e3efe1
samyounger <sam_younger@hotmail.com>
2016-09-10 16:55:56 +0100

    added JWT to index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan      = require("morgan");
 const bodyParser  = require("body-parser");
 const mongoose    = require("mongoose");
 const cors        = require("cors");
+const expressJWT  = require("express-jwt");
 
 const app       = express();
 const config    = require("./config/config");
@@ -16,6 +17,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
+
+app.use("/api", expressJWT({ secret: config.secret })
+  .unless({
+    path: [
+      { url: "/api/register", methods: ["POST"]},
+      { url: "/api/login", methods: ["POST"]}
+    ]
+  }));
+  app.use(jwtErrorHandler);
+
+function jwtErrorHandler(err, req, res, next) {
+  if(err.name !== "UnauthorizedError") return next();
+  return res.status(401).json({ message: "Unauthorized Request"});
+}
 
 app.use("/", webRouter);
 app.use("/api", apiRouter);
