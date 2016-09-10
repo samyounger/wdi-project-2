b81f58164b8889a5877eea0891fc29233eee1920
samyounger <sam_younger@hotmail.com>
2016-09-10 16:50:47 +0100

    authentications.js routes written

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
 
 const authentications = require("../controllers/authentications");
-const bars = require("../controllers/bars");
-
-// Page to show bars listed in the barpleeze database
-router.route("/bars")
-  .get(bars.index);
+const bars            = require("../controllers/bars");
+const users           = require("../controllers/users");
 
 // Authentication for creating and logging in users
 router.route("/register")
   .post(authentications.register);
 router.route("/login")
   .post(authentications.login);
+
+// User routes
+router.route("/users")
+  .get(users.index);
+router.route("/users/:id")
+  .get(users.show)
+  .put(users.update)
+  .delete(users.delete);
+
+// Page to show bars listed in the barpleeze database
+router.route("/bars")
+  .get(bars.index);
 
 module.exports = router;
--- controllers/authentications.js
+++ controllers/authentications.js
@@ -1,20 +1,40 @@
 module.exports = {
-  register: usersRegister,
-  login:    usersLogin
+  register: authenticationsRegister,
+  login:    authenticationsLogin
 };
 
-const User = require("../models/user");
+const User    = require("../models/user");
+const jwt     = require("jswebtoken");
+const config  = require("../config/config");
 
-function usersRegister(req, res) {
+function authenticationsRegister(req, res) {
   User.create(req.body.user, (err, user) => {
     if(err) return res.status(500).json({ message: "Something went wrong" });
-    return res.status(201).json({ user });
+
+    let token = jwt.sign(user._id, config.secret, { expiresIn: 60*60*24 });
+
+    return res.status(201).json({
+      message: `Welcome ${user.username}!`,
+      user,
+      token
+    });
   });
 }
 
-function usersLogin(req, res) {
+function authenticationsLogin(req, res) {
   User.findOne({ username: req.body.username}, (err, user) => {
     if(err) return res.status(500).json({ message: "Something went wrong" });
-    return res.status(201).json({ username });
+
+    if(!user || !user.validatePassword(req.body.password)) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    let token = jwt.sign(user._id, config.secret, { expiresIn: 60*60*24 });
+
+    return res.status(200).json({
+      message: `Welcome back ${user.username}`,
+      user,
+      token
+    });
   });
 }
