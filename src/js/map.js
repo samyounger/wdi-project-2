9fafa18eadfefdf0a4fa00d61a06eb4c2cb71f4d
samyounger <sam_younger@hotmail.com>
2016-09-14 19:52:07 +0100

    further styling of the page and changing menu on the right to a dropdown.

--- src/js/map.js
+++ src/js/map.js
@@ -1,18 +1,18 @@
 (function(globals){
   if (!('App' in globals)) { globals.App = {}; }
 
-    // Remove Markers
-    globals.App.removeMarkers = function() {
-      for (var i = 0; i < globals.App.markers.length; i++) {
-        globals.App.markers[i].setMap(null);
-      }
-    };
+  // Remove Markers
+  globals.App.removeMarkers = function() {
+    for (var i = 0; i < globals.App.markers.length; i++) {
+      globals.App.markers[i].setMap(null);
+    }
+  };
 
-    globals.App.reviewsLoop = function(reviews) {
-      for (var i = 0; i < reviews.length; i++) {
-        $(".favourites").append(`
-          <p class="review"><strong>${reviews[i].author_name}</strong>   rating => ${reviews[i].rating}</p>
-          <p class="review">${reviews[i].text}</p>
+  globals.App.reviewsLoop = function(reviews) {
+    for (var i = 0; i < reviews.length; i++) {
+      $(".favourites").append(`
+        <p class="review"><strong>${reviews[i].author_name}</strong>   rating => ${reviews[i].rating}</p>
+        <p class="review">${reviews[i].text}</p>
         `);
       }
     };
@@ -26,10 +26,10 @@
         map: this.map,
         content: `
         <form class="favourites">
-          <h3>${bar.name}</h3>
-          <img src="https://maps.googleapis.com/maps/api/place/photo?maxheight=150&photoreference=${bar.photos[0].photo_reference}&key=AIzaSyByvlUTw9rHtlxIbic2gCVdhpj-8dK7sTQ">
-          <p><strong>Phone:</strong> ${bar.formatted_phone_number}</p>
-          <p class="webAddress"><strong>WebSite:</strong> <a href="${bar.website}">${bar.website}</a></p>
+        <h3>${bar.name}</h3>
+        <img class="barImage" src="https://maps.googleapis.com/maps/api/place/photo?maxheight=150&photoreference=${bar.photos[0].photo_reference}&key=AIzaSyByvlUTw9rHtlxIbic2gCVdhpj-8dK7sTQ">
+        <p><strong>Phone:</strong> ${bar.formatted_phone_number}</p>
+        <p class="webAddress"><strong>WebSite:</strong> <a href="${bar.website}">${bar.website}</a></p>
         </form>`
       });
       globals.App.reviewsLoop(bar.reviews);
@@ -47,7 +47,7 @@
           globals.App.addInfoWindowForBar(data, marker);
         });
       });
-      $(".barNamesContainer").append(`<li class="list-group-item" id="${place.place_id}">${place.name}</li>`);
+      $(".barNamesContainer").append(`<li class="list-group-item" id="${place.place_id}"><a href="#">${place.name}</a></li>`);
       $(`#${place.place_id}`).on("click", marker, function() {
         let url = `http://localhost:3000/api/bar/${place.place_id}`;
         globals.App.ajaxRequest(url, "get", null, (data) => {
@@ -167,9 +167,6 @@
 
     // geoLocator
     globals.App.geoLocator = function() {
-      const infoWindow = new google.maps.InfoWindow({
-        map: globals.App.map
-      });
 
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -185,56 +182,53 @@
           });
           globals.App.map.setCenter(pos);
           globals.App.placesSetup(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, infoWindow.map.getCenter());
         });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, infoWindow.map.getCenter());
       }
     };
 
     // Setup the map with the intial starting options
     globals.App.mapSetup = function() {
-      $("main").html(`
-      <div class="row">
-        <div class="col-md-8">
+      $("main").html(
+        `
+        <div class="row mapSearchContainer">
           <div id="map-canvas"></div>
-        </div>
-        <div class="col-md-4">
-          <h2>Search for restaurants</h2>
-          <input type="text" id="searchBox" class="form-control" placeholder="Search">
-          <ul class="list-group barNamesContainer"></ul>
-        </div>
-      </div>`);
-      let canvas = document.getElementById('map-canvas');
-      globals.App.markers = [];
-      let lat = 51.506178;
-      let lng = -0.088369;
-      let mapLocation = new google.maps.LatLng(lat,lng);
-      let mapOptions = {
-        zoom: 15,
-        center: mapLocation,
-        scrollwheel: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles:
-        [{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"transit.station.airport","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"transit.station.airport","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#89cada"}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"}]}]
+          <div class="btn-group col-md-3 searchContainer open">
+            <input type="text" id="searchBox" class="form-control" placeholder="Search">
+            <button type="button" class="btn btn-default col-md-3 dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Action <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu barNamesContainer"></ul>
+          </div>
+        </div>`);
+
+        let canvas = document.getElementById('map-canvas');
+        globals.App.markers = [];
+        let lat = 51.506178;
+        let lng = -0.088369;
+        let mapLocation = new google.maps.LatLng(lat,lng);
+        let mapOptions = {
+          zoom: 15,
+          center: mapLocation,
+          scrollwheel: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          styles:
+          [{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"transit.station.airport","elementType":"labels.text.fill","stylers":[{"visibility":"on"}]},{"featureType":"transit.station.airport","elementType":"labels.icon","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#89cada"}]},{"featureType":"water","elementType":"geometry","stylers":[{"visibility":"on"}]}]
+        };
+        this.map = new google.maps.Map(canvas, mapOptions);
       };
-      this.map = new google.maps.Map(canvas, mapOptions);
-    };
 
-    globals.App.reRunPlaces = function() {
-      globals.App.removeMarkers();
-      let location = globals.App.map.getCenter();
-      globals.App.placesSetup(location);
-    };
+      globals.App.reRunPlaces = function() {
+        globals.App.removeMarkers();
+        let location = globals.App.map.getCenter();
+        globals.App.placesSetup(location);
+      };
 
-    globals.App.initMap = function(){
-      globals.App.mapSetup();
-      globals.App.geoLocator();
-      globals.App.searchBox();
-      $(".home").on("click", globals.App.mapSetup.bind(globals.App));
-      globals.App.map.addListener("dragend", globals.App.reRunPlaces);
-    };
+      globals.App.initMap = function(){
+        globals.App.mapSetup();
+        globals.App.geoLocator();
+        globals.App.searchBox();
+        $(".home").on("click", globals.App.mapSetup.bind(globals.App));
+        globals.App.map.addListener("dragend", globals.App.reRunPlaces);
+      };
 
-  })(window);
+    })(window);
