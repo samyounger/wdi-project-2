e63c0b99093c347be1c1d21c143210f8dc5969fd
samyounger <sam_younger@hotmail.com>
2016-09-16 11:22:40 +0100

    changed mongolab_uri to mongodb_uri

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGOLAB_URI || "mongodb://localhost/barpleeze",
+  db: process.env.MONGODB_URI || "mongodb://localhost/barpleeze",
   secret: process.env.SECRET || "secret token password"
 };
