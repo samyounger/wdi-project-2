4e85b7b249e3c8afd892298cfac5bc55c18da060
samyounger <sam_younger@hotmail.com>
2016-09-20 08:56:39 +0100

    some more tests added

--- test/api/bars_test.js
+++ test/api/bars_test.js
@@ -3,9 +3,15 @@ require("../spec_helper");
 const User = require("../../models/user");
 const Bar = require("../../models/bar");
 
-describe("GET /api/login with token", () => {
+let TOKEN;
+let bar;
+
+describe("Bar tests", () => {
 
   beforeEach(done => {
+    Bar.collection.drop();
+    User.collection.drop();
+
     const user = new User({
       username: "test",
       email: "test@test.com",
@@ -25,30 +31,39 @@ describe("GET /api/login with token", () => {
     });
   });
 
-  it("List bars: should return a 200 response", done => {
-    api
-      .get("/api/bars")
-      .set("Accept", "application/json")
-      .set("Authorization", `Bearer ${TOKEN}`)
+  describe("GET /api/bars", () => {
+    beforeEach(done => {
+      bar = new Bar({
+        name: "testBar",
+        googlePlaceId: "uniqueBarId",
+        url: "www.testbar.com",
+        lat: 15,
+        lng: 16
+      });
+      bar.save((err, bar) => {
+        done();
+      });
+    });
 
-      .expect(200, done);
+    it("GET bars.index: should return a 200 response", done => {
+      api.get("/api/bars")
+        .set("Accept", "application/json")
+        .set("Authorization", `Bearer ${TOKEN}`)
+        .expect(200, done);
+    });
+
+    // Complicated test. Requires access to the sub-array of the user object. Might also not be working because it needs access to googleMapsAPI to authenticate the key
+    // it("Get bars/:id.data: should return a 200 response", done => {
+    //   api.get(`/api/bar/${bar.id}`)
+    //     .set("Accept", "application/json")
+    //     .set("Authorization", `Bearer ${TOKEN}`)
+    //     .expect(200, done);
+    // });
+
+    it("POST register: should return a 201 response", done => {
+      api.get("/api/register")
+        .set("Accept", "application/json")
+        .expect(201, done);
+    });
   });
-
-  it("Create bar: should return a 200 response", done => {
-    api
-      .post("/users/:id/bars")
-      .set("Accept", "application/json")
-      .set("Authorization", `Bearer ${TOKEN}`)
-      .send({
-        bar: {
-          name: "testBar",
-          googlePlaceId: "testId",
-          url: "www.testsite.com",
-          lat: 15,
-          lng: 15
-        }
-      }).expect(200, done);
-  });
-
-
 });
