d6071939627009dfc9159daee68bb732dc76c51d
samyounger <sam_younger@hotmail.com>
2016-11-02 11:15:20 +0000

    added mLab reference for heroku in the config.js file for the db

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ module.exports = {
   db: {
     test: "mongodb://localhost/barpleeze-api-test",
     development: "mongodb://localhost/barpleeze-api-development",
-    production: process.env.MONGODB_URI || "mongodb://localhost/barpleeze-api-production"
+    // production: process.env.MONGODB_URI || "mongodb://localhost/barpleeze-api-production"
+    production: process.env.MONGODB_URI || "mongodb://heroku_4xghs6kc:7ae8c35ihf4oksf78c53gcf5d5@ds033086.mlab.com:33086/heroku_4xghs6kc"
   },
   secret: process.env.SECRET || "secret token password"
 };
