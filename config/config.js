d49a0d9ab5a9632a12e314209d25867962031578
samyounger <sam_younger@hotmail.com>
2016-11-02 11:27:04 +0000

    forgot to run gulp to push JS files to public

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    test: "mongodb://localhost/barpleeze-api-test",
-    development: "mongodb://localhost/barpleeze-api-development",
+    // test: "mongodb://localhost/barpleeze-api-test",
+    // development: "mongodb://localhost/barpleeze-api-development",
     // production: process.env.MONGODB_URI || "mongodb://localhost/barpleeze-api-production"
     production: process.env.MONGODB_URI || "mongodb://heroku_4xghs6kc:7ae8c35ihf4oksf78c53gcf5d5@ds033086.mlab.com:33086/heroku_4xghs6kc"
   },
